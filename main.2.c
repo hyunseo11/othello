515514434171f104449f3122906269ed2f5c65cd
hyunseo11 <70734028+hyunseo11@users.noreply.github.com>
2020-11-17 22:15:04 +0900

    02

--- main.2.c
+++ main.2.c
@@ -7,23 +7,25 @@ int othello() {  //배치상태출력
  
 	int s, t;
 
-    while() {
-		printf("----a--b--c--d--e--f--g--h\n");
-		for(s=0;s<8;s++){
-			printf("%d", s);}
-		for(t=0;t<8;t++){
-			printf("%c", gameboard[s][t]);
-			printf("\n");
-		}
+		printf("  0 1 2 3 4 5 \n"); 
+		for(s=0;s<6;s++) {
+			printf("----------------\n"); 
+			printf("%d|", s);
+		for(t=0;t<6;t++) {
+			printf("%c|", gameboard[s][t]);
+		  }
+	
+	    printf("\n");
+	    
 }
 }
+
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
 	int result;
 	
 	result = othello();
-	printf("흰색 돌 놓을 위치를 선택하세요\n");
 	
 	
 	return 0;
