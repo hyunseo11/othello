f54815b5ff45e33b5134eaeb627897431bbf963b
hyunseo11 <70734028+hyunseo11@users.noreply.github.com>
2020-11-17 20:56:00 +0900

    01

--- main1.c
+++ main1.c
@@ -4,24 +4,23 @@
 int gameboard[N][N];
 void init_othello() { //게임초기화
     int i, j;
+    
 	for(i=0;i<N;i++) {
 		for(j=0;j<N;j++) {
 		gameboard[i][j]=0;
 	}
 	 }   
+    gameboard[3][3] = 'B';
+    gameboard[3][4] = 'W';
+    gameboard[4][3] = 'W';
+    gameboard[4][4] = 'B';
 }
-
-
- 
-
-
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int i, j;
 	
-	init_othello(i, j);
+	init_othello();
     
 	return 0;
 }
